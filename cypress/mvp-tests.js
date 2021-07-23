fd6622ba0667187a172012bb3b44bdfb7ba7b911
kennymiesner <kennymiesner@gmail.com>
2021-07-23 08:04:51 -0500

    Installed Cypress and started writing MVP tests

---
+++ cypress/mvp-tests.js
@@ -0,0 +1,20 @@
+// Cypress MVP Tests
+
+describe('Cypress MVP Tests', {
+
+  // 1. Test that you can add text to the box
+  it('Can add text to the box', () => {
+
+  })
+
+  // 2. Test that you can select multiple toppings
+  it('Can select multiple toppings', () => {
+    
+  })
+
+  // 3. Test that you can submit the form
+  it('Can submit the form', () => {
+    
+  })
+  
+})
